56709493751eaa17a1309afc09ca3864e0d307d6
Jakub Čábera <cabera.jakub@gmail.com>
2020-05-20 12:26:50 +0200

    feat: Initial yaml dependency

---
+++ bin/update-supporting.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Update supporting tools to the latest version.
+.PARAMETER Supporting
+    Name of supporting tool to be updated.
+#>
+param([String] $Supporting = '*')
+
+'decompress', 'manifest', 'install' | ForEach-Object {
+    . "$PSScriptRoot\..\lib\$_.ps1"
+}
+
+$Sups = (Get-ChildItem "$PSScriptRoot\..\supporting\*" -File -Include "$Supporting.json").FullName
+
+foreach ($sup in $Sups) {
+    $name = ((Split-Path $sup -Leaf) -split '\.')[0]
+    $folder = Split-Path $sup -Parent
+    $dir = "$folder\$name\bin"
+
+    Write-Host "Updating $name" -ForegroundColor Magenta
+
+    Invoke-Expression "$PSScriptRoot\checkver.ps1 -App $name -Dir $folder -Update"
+    $manifest = parse_json $sup
+    if (!(Test-Path $dir)) { New-Item $dir -ItemType Directory | Out-Null }
+
+    $fname = dl_urls $name $manifest.version $manifest '' default_architecture $dir $true $true
+    # Pre install is enough now
+    pre_install $manifest $architecture
+
+    Write-Host "$name done" -ForegroundColor Green
+}
