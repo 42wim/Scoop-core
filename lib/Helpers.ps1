f782415f344489ac0273681218806c7dea7645c0
Jakub Čábera <cabera.jakub@gmail.com>
2020-10-18 10:01:16 -0700

    fix: $Host.UI.RawUI.ForegroundColor not working in Linux

--- lib/Helpers.ps1
+++ lib/Helpers.ps1
@@ -35,7 +35,7 @@ function Write-UserMessage {
         [Switch] $Success,
         [Switch] $SkipSeverity,
         [Alias('ForegroundColor')]
-        [System.ConsoleColor] $Color = $(if ($Host.UI.RawUI.ForegroundColor) { $Host.UI.RawUI.ForegroundColor } else { 'White' })
+        [System.ConsoleColor] $Color = $(if ($Host.UI.RawUI.ForegroundColor -and !$IsLinux) { $Host.UI.RawUI.ForegroundColor } else { 'White' })
     )
 
     begin {
