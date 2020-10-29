792d7be50dc370449613c02bc5c2422d3547257a
Jakub Čábera <cabera.jakub@gmail.com>
2020-10-29 13:51:36 -0700

    fix(Search): Allow empty colllection
    - Closes #72

--- lib/Helpers.ps1
+++ lib/Helpers.ps1
@@ -25,6 +25,7 @@ function Write-UserMessage {
     param(
         [Parameter(Mandatory, ValueFromPipeline, ValueFromRemainingArguments, Position = 0)]
         [AllowEmptyString()]
+        [AllowEmptyCollection()]
         [String[]] $Message,
         [ValidateSet('Message', 'Info', 'Warning', 'Error', 'Success')]
         [String] $Severity = 'Message',
